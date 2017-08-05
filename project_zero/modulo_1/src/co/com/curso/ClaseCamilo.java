f8eda8a86a207945ae9f06a21f511b377566d397
alejcacs <alejandro.carcamo@ingeneo.com.co>
2017-08-05 10:55:46 -0500

    OK

--- project_zero/modulo_1/src/co/com/curso/ClaseCamilo.java
+++ project_zero/modulo_1/src/co/com/curso/ClaseCamilo.java
@@ -1,3 +1,4 @@
 public class UnArchivo {
 	// Se crea una clase nueva
+	// Se adiciona una nueva línea
 }
--- project_zero/modulo_1/src/co/com/curso/UnArchivocopia.java
+++ project_zero/modulo_1/src/co/com/curso/UnArchivocopia.java
@@ -2,4 +2,5 @@ public class UnArchivo {
 	private String nombre;
 	private String ubicacion;
 	private String telefono;
+	public int edad;
 }
